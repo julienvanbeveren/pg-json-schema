74521ea50e2d70d3a4c5bd500f2b9e7ccfecfc57
Julien Van Beveren <julien.vanbeveren@gmail.com>
2024-03-15 22:57:48 +0100

    minimum maximum working

--- src/validate_schema.sql
+++ src/validate_schema.sql
@@ -3,14 +3,29 @@ RETURNS BOOLEAN AS $$
 BEGIN
 
   IF schema->>'type' = 'number' THEN
+
     IF NOT jsonb_typeof(data) = 'number' THEN
       RETURN FALSE;
     END IF;
+
     IF schema->>'multipleOf' IS NOT NULL THEN
       IF (data::NUMERIC % (schema->>'multipleOf')::NUMERIC) != 0 THEN
         RETURN FALSE;
       END IF;
     END IF;
+
+    IF schema->>'minimum' IS NOT NULL THEN
+      IF data::NUMERIC < (schema->>'minimum')::NUMERIC THEN
+        RETURN FALSE;
+      END IF;
+    END IF;
+
+    IF schema->>'maximum' IS NOT NULL THEN
+      IF data::NUMERIC > (schema->>'maximum')::NUMERIC THEN
+        RETURN FALSE;
+      END IF;
+    END IF;
+
   END IF;
 
   RETURN TRUE;

359c4d2988d3c59c63a61748134cbd45276e6a6f
Julien Van Beveren <julien.vanbeveren@gmail.com>
2024-03-15 22:37:45 +0100

    some progress on number

--- src/validate_schema.sql
+++ src/validate_schema.sql
@@ -1,9 +1,15 @@
 CREATE OR REPLACE FUNCTION validate_schema(data jsonb, schema jsonb)
 RETURNS BOOLEAN AS $$
 BEGIN
+
   IF schema->>'type' = 'number' THEN
     IF NOT jsonb_typeof(data) = 'number' THEN
       RETURN FALSE;
+    END IF;
+    IF schema->>'multipleOf' IS NOT NULL THEN
+      IF (data::NUMERIC % (schema->>'multipleOf')::NUMERIC) != 0 THEN
+        RETURN FALSE;
+      END IF;
     END IF;
   END IF;
 
